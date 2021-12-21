fefa8e9a305e31dee65bec40103f38b8e39219cf
Artem Pershutkin <79787099+Artom-art@users.noreply.github.com>
2021-12-21 22:36:29 +0300

    Add files via upload

--- src/scripts/state.ts
+++ src/scripts/state.ts
@@ -1,37 +1,79 @@
+import { getId } from "./structure"
+import { getDefaultText } from "./structure"
+
 export const state = 
 {
-    title: 'Name of presenatation',
-    slides: [
-        {
-        id: '1',
-        background: '#fff',
+    title: 'Name of presentation',
+    slides: [{
+        id: getId(),
+        background: '#FFFFFF',
+        content: [getDefaultText()]
+    }, {
+        id: getId(),
+        background: '#FF00FF',
         content: [
-            {   id: '1',
-                type: 'text',
+            getDefaultText(),
+            {
+                id: getId(),
+                type: 'circle',
+                primitive: 'circle',
                 position: {
-                    x: 50,
-                    y: 50
+                    x: 300,
+                    y: 300
                 },
                 size: {
-                    width: 100,
-                    height: 100
+                    width: 150,
+                    height: 150
                 },
-                font: '',
-                fontSize: 14,
-                weight: 'normal',
-                color: 'FFFFFF',
-                content: 'Текст слайда отображаться'}
+
+            }
         ]
-    },
-    {
-        id: '2',
-        background: '#fff',
-        content: []
-    },
-    {
-        id: '3',
-        background: '#fff',
-        content: []
-    }
-]
+    }, {
+        id: getId(),
+        background: '#32CD32',
+        content: [{
+            id: getId(),
+            type: 'text',
+            position: {
+                x: 150,
+                y: 150
+            },
+            size: {
+                width: 100,
+                height: 100
+            },
+            font: 'fantasy',
+            fontSize: 24,
+            weight: 400,
+            color: '#FF00FF',
+            content: 'New text'
+        }, {
+            id: getId(),
+            type: 'rectangle',
+            primitive: 'rectangle',
+            position: {
+                x: 50,
+                y: 50
+            },
+            size: {
+                width: 60,
+                height: 100
+            }
+        }]
+    }, {
+        id: getId(),
+        background: '#191970',
+        content: [{
+            id: getId(),
+            type: 'triangle',
+            position: {
+                x: 200,
+                y: 250
+            },
+            size: {
+                width: 200,
+                height: 100
+            }
+        }]
+    }]
 }
